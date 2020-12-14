3d9cb576e0c3825a3e2918869fe256e1755d7dec
kevin king <it.zhangsijian@gmail.com>
2020-12-14 16:40:45 +0800

    error

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e 
-supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
-/usr/sbin/crond   -f  -L  /var/log/cron/cron.log
-cp -f $CODE_PATH/$PHP_ENV_FILE $CODE_PATH/.env
-chmod -R 777 $CODE_PATH/storage
+#cp -f $CODE_PATH/$PHP_ENV_FILE $CODE_PATH/.env
+#chmod -R 777 $CODE_PATH/storage
 
+#/usr/sbin/crond   -f  -L  /var/log/cron/cron.log
+supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
 
