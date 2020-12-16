a2f60ad7cfb57ff8bb85fa54ca1e29ddc6338da4
kevin king <it.zhangsijian@gmail.com>
2020-12-16 14:12:50 +0800

    chmod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e 
 cp -f $CODE_PATH/$PHP_ENV_FILE $CODE_PATH/.env
-chmod -R 777 $CODE_PATH/storage/logs/
+chmod -R 777 $CODE_PATH/storage
 
 #/usr/sbin/crond   -f  -L  /var/log/cron/cron.log
 supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
